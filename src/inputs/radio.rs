f773c52f9574ccbeef51f1b2e85272bdc2fe95b3
Fabian Keller <github.100.fkeller@spamgourmet.com>
2024-10-29 11:42:19 +0100

    added input parsing unit tests; proper support for arbitrary types in radio

--- src/inputs/radio.rs
+++ src/inputs/radio.rs
@@ -1,5 +1,5 @@
 use pyo3::prelude::*;
-use pyo3::types::PyList;
+use pyo3::types::PySequence;
 
 /// Wrapper newtype for the underlying PyObject instance.
 #[derive(Debug)]
@@ -14,7 +14,7 @@ impl PyRadio {
     }
     pub fn set_to_index(&self, py: Python<'_>, index: usize) -> PyResult<()> {
         let py_radio = self.0.bind(py);
-        let values = py_radio.getattr("_values")?.downcast_into::<PyList>()?;
+        let values = py_radio.getattr("_values")?.downcast_into::<PySequence>()?;
         py_radio.setattr("_value", values.get_item(index)?)
     }
 }
@@ -35,9 +35,16 @@ impl<'py> FromPyObject<'py> for Radio {
         let name: String = obj.getattr("_name")?.extract()?;
         let init_index: usize = obj.getattr("_init_index")?.extract()?;
 
-        // TODO: We probably have to make explicit __str__ calls to the elements of
-        // the list here.
-        let value_names: Vec<String> = obj.getattr("_values")?.extract()?;
+        // Note that a radio supports arbitrary underlying types, and we are using `__str__` calls to
+        // infer the label strings.
+        let mut value_names = Vec::<String>::new();
+        let raw_values = obj.getattr("_values")?;
+        let raw_values = raw_values.downcast::<PySequence>()?;
+        for raw_value in raw_values.iter()? {
+            let raw_value = raw_value?;
+            let value_name: String = raw_value.call_method("__str__", (), None)?.extract()?;
+            value_names.push(value_name);
+        }
 
         Ok(Radio {
             name,
--- src/py_module.rs
+++ src/py_module.rs
@@ -1,7 +1,7 @@
 use pyo3::prelude::*;
 use pyo3::types::{PyFunction, PyList};
 
-use super::inputs::Inputs;
+use super::inputs::{Input, Inputs};
 use super::main_run_ui::run_ui;
 
 #[pyfunction]
@@ -11,8 +11,18 @@ fn run(inputs: &Bound<'_, PyList>, callback: &Bound<'_, PyFunction>) -> PyResult
     Ok(())
 }
 
+/// Exposing the input parsing is currently only needed for unit testing.
+/// TODO: Figure out a way how to test the "value setting" part as well.
+#[pyfunction]
+#[pyo3(name = "_parse_input")]
+fn parse_input(_input: &Bound<'_, PyAny>) -> PyResult<()> {
+    let _input: Input = _input.extract()?;
+    Ok(())
+}
+
 #[pymodule]
 fn _picoapp(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(run, m)?)?;
+    m.add_function(wrap_pyfunction!(parse_input, m)?)?;
     Ok(())
 }
